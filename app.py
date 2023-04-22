cb1a07995b7408ad97e0e1d591e510b9c63a4582
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2023-04-22 12:00:00 +0100

    Add input ouput colors

--- app.py
+++ app.py
@@ -7,6 +7,24 @@ config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
 
+def bold(text):
+    bold_start = "\033[1m"
+    bold_end = "\033[0m"
+    return bold_start + text + bold_end
+
+
+def blue(text):
+    blue_start = "\033[34m"
+    blue_end = "\033[0m"
+    return blue_start + text + blue_end
+
+
+def red(text):
+    red_start = "\033[31m"
+    red_end = "\033[0m"
+    return red_start + text + red_end
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Simple command line chatbot with GPT-4"
